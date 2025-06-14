754f3bee77baea2340c3ed7635b2c714f3e823ed
sebastian.zapata <sebitaszapata12@gmail.com>
2025-06-13 21:05:57 -0500

    implementation of fizz buzz

--- src/main/scala/part2datastreams/EssentialStream.scala
+++ src/main/scala/part2datastreams/EssentialStream.scala
@@ -51,9 +51,48 @@ object EssentialStream {
     env.execute()
   }
 
+  def fizzbuzz(): Unit= {
+    /**
+     * exercise : fizzbuzz on flink
+     * - take a stream of 100 natural number
+     * - for every number
+     *  - if n%3 == then return "fizz"
+     *  - if n%5 == then "buzz"
+     *  - if both => "fizzbuzz"
+     * - print the number for wich you said "fizzbuzz" to a file or in the console.
+     */
+
+    // 1 - execution enviroment
+    val env: StreamExecutionEnvironment = StreamExecutionEnvironment.getExecutionEnvironment
+
+    //computations in between
+    import org.apache.flink.streaming.api.scala._ // import TypeInformation for the data of your datastreams
+    val input: DataStream[Int] = env.fromSequence(1,100).map(_.toInt).map{
+      it =>
+        var fizzbuzz=""
+        if(it%3==0){
+          fizzbuzz += "fizz"
+        }
+        if(it%5==0){
+          fizzbuzz+="buzz"
+        }
+        Tuple2.apply(it,fizzbuzz)
+    }.filter( it => it._2 =="fizzbuzz").map(_._1)
+
+    val file = input.writeAsText("output/fizzbuzz.txt")
+    file.setParallelism(2)
+
+    val print = input.print()
+    // it print the thread number and then the number itself
+
+    // a the end
+    env.execute()
+  }
+
+
 
   def main(args: Array[String]): Unit = {
-    demoTransformation()
+    fizzbuzz()
   }
 
 }
